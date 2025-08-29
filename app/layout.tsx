45bb8849c16c6f69dc51ae06ee0b2016b4191ca3
Xavi <ensable@gmail.com>
2025-08-29 12:58:59 +0200

    chore: remove unnecesary

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,3 @@
-import { GeistSans } from "geist/font/sans";
-import { GeistMono } from "geist/font/mono";
 import "./globals.css";
 
 export default function RootLayout({
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-// This file is only for v0 preview compatibility
-// In the actual Expo project, App.tsx is the entry point
-import App from "./App";
-
-export default function Page() {
-  return <App />;
-}
