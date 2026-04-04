3c646c534ef332434a05591438405eda36e0efdc
Xavier Rodríguez <ensable@gmail.com>
2026-04-04 22:25:58 +0200

    Add game configuration constants for multiplayer Asteroids

---
+++ server/src/lib/GameConstants.ts
@@ -0,0 +1,14 @@
+// Game configuration constants for multiplayer Asteroids
+
+const GAME_CONSTANTS = {
+    SCREEN_WIDTH: 800,
+    SCREEN_HEIGHT: 600,
+    SHIP_SPEED: 5,
+    ASTEROID_SPEED: 2,
+    BULLET_SPEED: 10,
+    MAX_ASTEROIDS: 50,
+    MAX_PLAYERS: 4,
+    GAME_TICK: 60 // milliseconds
+};
+
+export default GAME_CONSTANTS;
